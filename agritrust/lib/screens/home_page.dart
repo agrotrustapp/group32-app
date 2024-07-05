8396dcf4437cfa4c0ef120bb23c4b4c8000a6939
WAMALA ABDUL JUNIOR <wamla.abdul.junior@gmail.com>
2024-07-05 05:42:58 -0700

    editing

--- agritrust/lib/screens/home_page.dart
+++ agritrust/lib/screens/home_page.dart
@@ -3,31 +3,26 @@ import 'package:flutter/material.dart';
 // import 'package:agrochemicalsellers/screens/seller_list_screen.dart';
 
  
-class HomePage extends StatelessWidget {
+ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage('lib/images/agrochemicals.jpg'),
-          fit: BoxFit.cover,
-        ),
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 136, 221, 213),
+      appBar: AppBar(
+        title: const Text('Agrotrust'),
       ),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('AgriTrust App'),
-        ),
-        backgroundColor: Colors.transparent, // make Scaffold transparent
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Connect with trusted Agrochemical Sellers',
-                style: TextStyle(fontSize: 24),
-              ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Connect with trusted Agrochemical Sellers',
+              style: TextStyle(fontSize: 24),
+            ),
+
+              
               const SizedBox(height: 20),
               ElevatedButton(
                 child: const Text(
@@ -37,65 +32,99 @@ class HomePage extends StatelessWidget {
                 onPressed: () {
                   Navigator.push(
                     context,
-                    MaterialPageRoute(builder: (context) => const SellerListScreen()),
+                    MaterialPageRoute(builder: (context) => const SellerListScreen(sellers: [],)),
                   );
                 },
               ),
             ],
           ),
         ),
-      ),
-    );
+      );
   }
 }
 
+class SellerListScreen extends StatelessWidget {
+  final List<Seller> sellers;
 
-
-class SellerListScreen extends StatefulWidget {
-  const SellerListScreen({super.key});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _SellerListScreenState createState() => _SellerListScreenState();
-}
-
-class _SellerListScreenState extends State<SellerListScreen> {
-  List<Seller> sellers = [
-    Seller(name: 'AgroChem Enterprises', location: 'Kampala', rating: 4.8),
-    Seller(name: 'Growers LLC', location: 'mukono', rating: 4.5),
-    Seller(name: 'Planters Inc', location: 'Nansana', rating: 4.2),
-    Seller(name: 'Greenway Agro', location: 'jinja', rating: 4.0),
-    Seller(name: 'CropKing Farms', location: 'masaka', rating: 4.0),
-  ];
+  const SellerListScreen({super.key, required this.sellers});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 146, 206, 200),
       appBar: AppBar(
-        title: const Text('Trusted Agrochemical Sellers'),
+        title: const Text('Sellers'),
       ),
-      backgroundColor: const Color.fromARGB(255, 122, 192, 124),
-      body: Container(
-        padding: const EdgeInsets.all(16),
-        child: ListView.builder(
-          itemCount: sellers.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(sellers[index].name),
-              subtitle: Text(sellers[index].location),
-              trailing: Text('${sellers[index].rating}'),
-            );
-          },
-        ),
+      body: ListView.builder(
+        itemCount: sellers.length,
+        itemBuilder: (context, index) {
+          final seller = sellers[index];
+          return ListTile(
+            leading: Image.asset(seller.image),
+            title: Text(seller.name),
+            subtitle: Text(seller.address),
+            trailing: Text('${seller.rating}'),
+          );
+        },
       ),
     );
   }
 }
+  
 
-class Seller {
-  final String name;
-  final String location;
-  final double rating;
 
-  Seller({required this.name, required this.location, required this.rating});
+
+ class Sellers {
+  static final List<Seller> _sellers = [
+    Seller(
+      name: 'ABC Agrochemicals Ltd.',
+      image: 'assets/images/agro1.jpg',
+      address: 'KAMPALA',
+      rating: 4.5,
+      products: ['Insecticides', 'Fungicides', 'Herbicides'],
+    ),
+    Seller(
+      name: 'DEF Agrochemicals Ltd.',
+      image: 'assets/images/agro2.jpg',
+      address: 'MUKONO',
+      rating: 3.8,
+      products: ['Fungicides', 'Herbicides'],
+    ),
+    Seller(
+      name: 'GHI Agrochemicals Ltd.',
+      image: 'assets/images/agro3.jpg',
+      address: 'MASAKA',
+      rating: 4.2,
+      products: ['Insecticides', 'Herbicides'],
+    ),
+    // Add more Seller objects here
+    Seller(
+      name: 'JKL Agrochemicals Ltd.',
+      image: 'assets/images/agro4.jpg',
+      address: 'WAKISO',
+      rating: 4.1,
+      products: ['Fungicides', 'Herbicides'],
+    ),
+  ];
+
+  static List<Seller> getSellers() {
+    return _sellers;
+  }
 }
+class Seller {
+  final String name; // Name of the seller
+  final String image; // URL or path to the seller's image
+  final String address; // Address of the seller
+  final double rating; // Average rating of the seller (out of 5.0)
+  final List<String> products; // List of products the seller sells
+
+  Seller({
+    required this.name,
+    required this.image,
+    required this.address,
+    required this.rating,
+    required this.products,
+  });
+}
+
+ 

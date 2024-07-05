3129b692d77d7005cb8ed2d5741535c85305e5e0
agrotrustapp <oscartyson132@gamial.com>
2024-07-05 04:22:44 -0700

    designing home page

--- agritrust/lib/main.dart
+++ agritrust/lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: const HomePage(), // Replace with your initial screen
+      home: HomePage(), // Replace with your initial screen
     );
   }
 }
